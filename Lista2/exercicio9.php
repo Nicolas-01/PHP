7e5380c8da8d8c2b4978597cc28b19da6f6c8c7d
Nicolas Santos <santosnicolas026@gmail.com>
2025-02-26 08:28:14 -0300

    Lista 2 - Exercício 9

---
+++ Lista2/exercicio9.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício 9</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <h1>Exercício 9</h1>
+
+    <form method="post" action="exercicio9resposta.php">
+        <div class="mb-3">
+            <label for="valor1" class="form-label">Insira o raio do círculo:</label>
+            <input type="number" id="valor1" name="valor1" class="form-control" required="">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
