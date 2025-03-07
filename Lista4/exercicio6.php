13ccd8c7213af7ac638d045ed436eab5557b9086
Nicolas Santos <santosnicolas026@gmail.com>
2025-03-07 09:48:20 -0300

    Lista 4 - Exercício 6

---
+++ Lista4/exercicio6.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício 6</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <h1>Exercício 6</h1>
+
+    <p>Digite um número de ponto flutuante para arredondar (Ex: 4.9)</p>
+
+    <form method="POST" action="exercicio6resposta.php">
+        <div class="mb-3">
+            <label for="numero" class="form-label">Número:</label>
+            <input type="number" id="numero" name="numero" class="form-control" required step="0.01">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
