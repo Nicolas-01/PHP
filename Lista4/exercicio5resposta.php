e294eda9081731a2ee957b1d59aef83dfe500372
Nicolas Santos <santosnicolas026@gmail.com>
2025-03-07 09:48:14 -0300

    Lista 4 - Exercício 5

---
+++ Lista4/exercicio5resposta.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercicio 5 Resposta</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <h1>Resposta do Exercício 5</h1>
+    <?php
+
+    function RaizQuadrada(float $num): float
+    {
+        return $num ** 2;
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        try {
+            $num = $_POST['numero'];
+            $valor = RaizQuadrada($num);
+            echo "A raíz quadrada de $num é $valor.";
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
